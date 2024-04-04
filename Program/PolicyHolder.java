b0f945b4c41670020605df1805414d6bad4658e0
NathanPham19 <s3940676@rmit.edu.vn>
2024-04-04 09:09:22 +0700

    Testing the main program and the loading of data

--- Program/PolicyHolder.java
+++ Program/PolicyHolder.java
@@ -2,10 +2,10 @@ package Program;
 
 import java.util.List;
 
-public class PolicyHolders extends Customer{
+public class PolicyHolder extends Customer{
     private List<Customer> dependents;
 
-    public PolicyHolders(String id, String fullName, InsuranceCard insuranceCard, List<Claim> claims) {
+    public PolicyHolder(String id, String fullName, InsuranceCard insuranceCard, List<Claim> claims) {
         super(id, fullName, insuranceCard, claims);
     }
 
